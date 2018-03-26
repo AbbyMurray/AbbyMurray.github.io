8bbc986a70e496f852df4ea2c86983752f2017e5
AbbyMurray <35699983+AbbyMurray@users.noreply.github.com>
2018-03-26 01:27:27 -0400

    Add files via upload

---
+++ AlarmClock/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+ 
+  setTimeout(function(){
+    $(".body").addClass("alarming"); 
+     $("body").addClass("alarming");
+  }, 5000);
+
+  $(".button").click(function() { 
+    $(".body").removeClass("alarming");
+    $("body").removeClass("alarming");
+  });
+
+});
